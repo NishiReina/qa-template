a5117a728f36ccd2f2add24d7b8efed84680fbc3
NishiReina <s1f101901195@iniad.org>
2022-04-27 23:20:56 +0900

    [Add] SlackController showDialog()

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -3,8 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class SlackController extends Controller
 {
     //
+    const BOT_TOKEN = 'xoxb-3431016278823-3439267906486-emBdL7Lz9DcJ0ZcabZkVL1Aq';
+
+    public function showDialog(Request $request){
+        $url = 'https://slack.com/api/dialog.open';
+        $token = self::BOT_TOKEN;
+        $dialog = $this->getDialog();
+        $trigger_id = $request->input('trigger_id');
+
+        $params = [
+            'token' => $token,
+            'dialog' => $dialog,
+            'trigger_id' => $trigger_id
+        ];
+
+        $client = new Client();
+
+        $response = $client->request(
+            'POST',
+            $url,
+            ['query' => $params]
+        );
+
+        $log = json_decode($response->getBody()->getContents(), true);
+        Log::info(print_r($log, true));
+
+        return response('',200);
+    }
+
+    // 引き継ぎテンプレートのテンプレートを作る
+    public function getDialog(){
+        return [
+            "callback_id" => "yukyu",
+            "title" => "休暇申請ダイアログ",
+            "submit_label" => "送信",
+            "state" => "none",
+            "elements" => [
+                [
+                    "type" => "select",
+                    "label" => "休暇種類",
+                    "name" =>  "holiday_type",
+                    "options" => [
+                        [
+                            "label" => "有給休暇",
+                            "value" => "type-full"
+                        ],
+                        [
+                            "label" => "午前休暇",
+                            "value" => "type-am"
+                        ],
+                        [
+                            "label" => "午後休暇",
+                            "value" => "type-pm"
+                        ]
+                    ],
+                ],
+                [
+                    "type" => "text",
+                    "label" => "休暇予定日",
+                    "name" => "date",
+                    "placeholder" => "2020-01-01",
+                ],
+                [
+                    "type" => "textarea",
+                    "label" => "休暇理由",
+                    "name" =>  "reason",
+                    "hint" => "理由を記入してください",
+                ]
+            ],
+        ];
+    }
+
 }
